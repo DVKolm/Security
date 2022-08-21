cab0c537d1c380c722a9b3a956d69e89043b7eaf
Vlad <git.vladislav.kolomytsev@gmail.com>
2022-08-21 18:43:05 +0300

    Security Application

---
+++ src/main/java/com/example/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,47 @@
+package com.example.rest;
+
+import com.example.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Some name", "Second name"),
+            new Developer(2L, "Some name", "Second name"),
+            new Developer(3L, "Some name", "Third name")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
+    public Developer create(Developer developer) {
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
+    public void deleteById(Long id) {
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+}
